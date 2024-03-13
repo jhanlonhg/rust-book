0a8f2723c23e2376f133bde29fed466ef73b9c15
John Hanlon <john.hanlon@hginsights.com>
2024-03-13 15:00:04 -0700

    Restaurant examples in single file

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,81 @@
+// crate
+//  └── front_of_house
+//      ├── hosting
+//      │   ├── add_to_waitlist
+//      │   └── seat_at_table
+//      └── serving
+//          ├── take_order
+//          ├── serve_order
+//          └── take_payment
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    // In scope via use keyowrd
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in summer with rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about the kind of bread
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
